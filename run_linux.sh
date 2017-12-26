6e5b01cdde94c42a9b13edafbf52f579e660f4e3
Radu Pogonariu <radu.pogonariu@gmail.com>
2017-12-26 21:13:30 +0200

    Fix Linux run script

--- run_linux.sh
+++ run_linux.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Installing requirements / checking for updates..."
 echo -e "\n"
-python3.6 -m pip install --upgrade -r requirements.txt
+sudo python3 -m pip install --upgrade -r requirements.txt
 echo -e "\n\n"
 echo "Running the bot..."
-python3.6 bot.py
+python3 bot.py
